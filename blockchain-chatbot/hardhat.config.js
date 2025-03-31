210d5f11e133209a76cf8df8c0e9310671641294
anthony-marr <am.marraccini216@gmail.com>
2025-03-31 15:30:57 -0600

    Updated setup

--- blockchain-chatbot/hardhat.config.js
+++ blockchain-chatbot/hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
     development: {
       url: "http://127.0.0.1:8545",
       chainId: 1337,
-      accounts: ["0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d"]
+      accounts: ["0x7f93199e58143eabc8ced3f06b0f051b8ecc8145a3879277ec44af072d202d3c"]
     }
   }
 };
